ad19392135612df3384cf44f223f28033fd3e078
Rokeya Sarker <133252712+rokeyasarker49@users.noreply.github.com>
2023-06-17 23:24:51 +0600

    Add files via upload

--- script.js
+++ script.js
@@ -36,11 +36,46 @@
         // number = number + 2;
 // }
 
-// for(number=0; number <= 20; number = number=2){
+// for(var number=0; number <= 20; number = number=2){
         // console.log(number);
 // }
   
-// for(number=1; number <= 20; number = number=2){
+// for(var number=1; number <= 20; number = number=2){
         // console.log(number);
+// }
+
+// var numbers = [10,20,30,40,50,60,70,80,90];
+
+//console.log(number.length);
+//console.log(numbers[30]);
+// var index = numbers.indexOf(70);
+// console.log(index);
+
+// for(var i = 0; i < numbers.length; i++){
+        // console.log(numbers[i]);
+        // var element = numbers [i];
+        // console.log(element);
+// }
+
+// var nameOfMyFriends = ["Shuchy", "Subah", "Arpita", "Bushra", "Akhi", "Mahee"];
+
+// for(var i = 0; i < nameOfMyFriends.length; i++){
+//        var element = nameOfMyFriends[i];
+        // console.log(element);
+//}
+
+// for(var i = 2; i < nameOfMyFriends.length; i++){
+        // var element = nameOfMyFriends[i];
+        // console.log(element);
+// }
+
+// for(var i = 0; i < nameOfMyFriends.length - 2; i++){
+        // var element = nameOfMyFriends[i];
+        // console.log(element);
+// }
+
+// for(var i = 0; i < nameOfMyFriends.length; i+=2){
+        // var element = nameOfMyFriends[i];
+        // console.log(element);
 // }
 
