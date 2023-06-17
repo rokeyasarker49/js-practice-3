1e7c59020703856bf046482676e9ccc48ea6eff6
Rokeya Sarker <133252712+rokeyasarker49@users.noreply.github.com>
2023-06-17 23:07:10 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,46 @@
+// console.log("OK");
+// console.log("OK");
+// console.log("OK");
+// console.log("OK");
+// console.log("OK");
+// console.log("OK");
+// console.log("OK");
+// console.log("OK");
+
+//While Loop
+
+// var learningProcess = 0;
+// while(learningProcess < 6){
+        // console.log("I am still learning");
+        //learningProcess = learningProcess + 1
+        //learningProcess += 1;
+        // learningProcess++;
+        // console.log(learningProcess);
+// }
+
+// var number = 1;
+// while(number <= 10){
+        // console.log(number);
+        // number++;
+// }
+
+// var number = 0;
+// while(number <=20){
+        // console.log(number);
+        // number = number + 2;
+// }
+
+// var number = 1;
+// while(number <=20){
+        // console.log(number);
+        // number = number + 2;
+// }
+
+// for(number=0; number <= 20; number = number=2){
+        // console.log(number);
+// }
+  
+// for(number=1; number <= 20; number = number=2){
+        // console.log(number);
+// }
+
